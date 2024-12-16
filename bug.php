ea923d44121febe916ab4db18b3af817339a693a
BugHunter-X <askzenai@gmail.com>
2024-12-16 11:28:04 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,6 @@
+function myFunc() {
+  //some code
+}
+
+myFunc(); // calling function without parentheses will cause a parse error
+myFunc; // this will not cause a parse error but won't call the function
